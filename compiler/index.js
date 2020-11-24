071808c6b8a1023c0c18dd62d0e7e5c2fb452f5d
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-24 12:17:16 +0600

    Update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -1,5 +1,5 @@
 let fs = require("fs");
 
-fs.readfile("test/test.lw", "utf-8", function(content) {
+fs.readfile("test/test.lw", "utf-8", function(error, content) {
    console.log(content)        
 });
