37987f6cced8720ad2a6ba761ef47a4240752570
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-24 23:38:02 +0600

    update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -1,5 +1,10 @@
 let fs = require("fs");
 
 fs.readFile("test/test.lw", "utf-8", function(error, content) {
-  console.log(content)
+  let text = content.replace(/\s\s+/gm, " ");
+  let strings = text.split(';');
+
+  for (let i = 0; I < strings.length; i++) {
+    let currentStrings = strings[i];
+  }
 });
