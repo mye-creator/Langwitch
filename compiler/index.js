3befe8c595140fe0773ae960c135861c8f89a50c
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-25 10:58:38 +0600

    update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -3,6 +3,11 @@ let fs = require("fs");
 let lexer = require("./modules/lexer.js").LEXER;
 
 fs.readFile("../test/test.lw", "utf-8", function(error, content) {
+  if (!!error) {
+    let lexems = lexems(content)
 
-  console.log(JSON.stringify(lexems, null, 4));
+    console.log(JSON.stringify(lexems, null, 4));
+  } else {
+    console.error("ERROR!!!")
+  }
 });
