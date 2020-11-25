b5a5f5cc241d702e6905462c1e71dba33f961264
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-25 10:23:36 +0600

    update lexer.js

--- compiler/modules/lexer.js
+++ compiler/modules/lexer.js
@@ -1,1 +1,13 @@
+let LEXER = function (content, dictionary) {
 
+  let text = content.replace(/\s\s+/gm, " ");
+  let strings = text.split(';');
+
+  let lexems = [];
+
+  // Our code here
+
+  return lexems;
+};
+
+module.exports.LEXER = LEXER;
