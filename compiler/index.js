f8cc72bce172e279e366d7d56ca872f52df36cc7
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-25 10:29:58 +0600

    update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -2,43 +2,46 @@ let fs = require("fs");
 
 let lexer = require("./modules/lexer.js").LEXER;
 
-fs.readFile("../test/test.lw", "utf-8", function(error, content) {
-  let text = content.replace(/\s\s+/gm, " ");
-  let strings = text.split(';');
+// fs.readFile("../test/test.lw", "utf-8", function(error, content) {
+//  let text = content.replace(/\s\s+/gm, " ");
+//  let strings = text.split(';');
 
-  let lexems = []
+//  let lexems = []
 
-  for (let i = 0; i < strings.length; i++) {
-    let currentString = strings[i].trim();
+//  for (let i = 0; i < strings.length; i++) {
+//    let currentString = strings[i].trim();
 
-    if (currentString !== "") {
-      let words = currentString.split(" ");
-      let stringObject = {};
+//    if (currentString !== "") {
+//      let words = currentString.split(" ");
+//      let stringObject = {};
 
-      for (let j = 0; j < words.length; j++) {
-        if (words[j].toLowerCase() === "print") {
-          stringObject["id"] = "print";
-        } else if (words[j].toLowerCase() === ":") {
-          stringObject["operator"] = ":";
-        } else {
-          let copyString = currentString;
-
-          if (/PRINT : \((.*)\)/gim.test(copyString)) {
-            let str = copyString.replace(/PRINT : \((.*)\)/gim, '$1');
-            stringObject["string"] = str;
-          } else if (/PRINT : (.*)/gim.test(copyString)) {
-            let num = copyString.replace(/PRINT : (.*)/gim, '$1');
-            stringObject["number"] = num;
-          }
-
-        }
-
-      }
-
-      lexems.push(stringObject);
-
-    }
-
-  }
-  console.log(lexems)
-});
+//      for (let j = 0; j < words.length; j++) {
+//        if (words[j].toLowerCase() === "print") {
+//           stringObject["id"] = "print";
+//          }
+//          else if (words[j].toLowerCase() === ":") {
+//           stringObject["operator"] = ":";
+//          }
+//
+//          else {
+//          let copyString = currentString;
+//
+//          if (/PRINT : \((.*)\)/gim.test(copyString)) {
+//            let str = copyString.replace(/PRINT : \((.*)\)/gim, '$1');
+//            stringObject["string"] = str;
+//          } else if (/PRINT : (.*)/gim.test(copyString)) {
+//            let num = copyString.replace(/PRINT : (.*)/gim, '$1');
+//            stringObject["number"] = num;
+//          }
+//
+//        }
+//
+//      }
+//
+//      lexems.push(stringObject);
+//
+//    }
+//
+//  }
+//  console.log(lexems)
+//});
