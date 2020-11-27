15329f24bb4d862e13573628ee4820258b97ea1f
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-27 07:47:45 +0600

    Update dictionary.js

--- compiler/modules/dictionary.js
+++ compiler/modules/dictionary.js
@@ -1,7 +1,19 @@
 let DICTIONARY = {
   "function" : {
     print : function (el) {
-       console.log(el);
+       let type = "%";
+      
+       if (el.type === "char") type += "c";
+       else if (el.type === "string") type += "s";
+       else if (el.type === "integer") type += "d";
+       else if (el.type === "float") type += "f";
+       else if (el.type === "undefined") type += "s";
+       else {
+          console.error("UNKNOWN_TYPE");
+          return;
+       }
+       
+       return `printf(${type}, ${el.value});`;
     }
   }
 };
