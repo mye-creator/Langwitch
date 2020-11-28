659b66dd74043576723db01a28c8d12d7d690a06
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-28 23:37:27 +0600

    Update dictionary.js

--- compiler/modules/dictionary.js
+++ compiler/modules/dictionary.js
@@ -1,25 +1,22 @@
 let Type = require("./utils/type.js").TYPE;
+let Eval = require("./utils/eval.js").EVAL;
 
 let DICTIONARY = {
   "function" : {
     print : function (type, val) {
        let typeSymbol = "%";
-       let parsedValue = value;
-       
-       if (type === "int" || type === "float") {
-          parsedValue = eval(parsedValue);
-         
-          if (Type(parsedValue) !== type) {
-             console.error("Type Error : " + typeof parsedValue + " !== " + type);
-             return;
-          }
+       let parsedValue = Eval(val);
+
+       if (Type(parsedValue) !== type) {
+          console.error("Type Error : " + typeof parsedValue + " !== " + type);
+          return;
        }
        
        if (type === "char") typeSymbol += "c";
-       else if (el.type === "string") typeSymbol += "s";
-       else if (el.type === "int") typeSymbol += "d";
-       else if (el.type === "float") typeSymbol += "f";
-       else if (el.type === "undefined") typeSymbol += "s";
+       else if (type === "string") typeSymbol += "s";
+       else if (type === "int") typeSymbol += "d";
+       else if (type === "float") typeSymbol += "f";
+       else if (type === "undefined") typeSymbol += "s";
        else {
           console.error("UNKNOWN_TYPE : " + type);
           return;
