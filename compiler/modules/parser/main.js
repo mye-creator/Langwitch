15a1f3efeb6ab28011502d442d510f30eb5b86d4
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-29 00:39:49 +0600

    Update main.js

--- compiler/modules/parser/main.js
+++ compiler/modules/parser/main.js
@@ -6,18 +6,18 @@ function parser (lexems, dictionary) {
    if (lexems && lexems.length !== 0) {
       for (let i = 0; i < lexems.length; i++) {
          let functionName = lexems[i]["function"];
-         let typeObj = lexems[i]["type"];
          let valueObj = lexems[i]["value"];
          if (lexems[i]["function"] && dictionary["function"][functionName.toLowerCase()]) {
             let currentFunction = dictionary["function"][functionName.toLowerCase()];
 
-            current = currentFunction(typeObj.value);
+            current = currentFunction(valueObj);
             start = start + current;
          }
          else {
-            console.error("function \""+functionName+"\" isn't registered!");
+            console.error("Function \""+functionName+"\" isn't registered!");
          }
        }
+
        return start + end;
    }
    else {
