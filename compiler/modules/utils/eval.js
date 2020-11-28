5960e21b6cedba42a7794572f5feee441ccce5e2
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-29 01:50:08 +0600

    Update eval.js

--- compiler/modules/utils/eval.js
+++ compiler/modules/utils/eval.js
@@ -1,20 +1,20 @@
 let Type = require("./type.js").TYPE;
 
 let Eval = function (value) {
-	if (Type(value) === "int" || Type(value) === "float") {
-        let evaled = eval(value);
+        if (Type.convert(value) === "int" || Type.convert(value) === "float") {
+                let evaled = eval(value);
 
-        if (typeof evaled === "number" && isFinite(evaled)) {
-        	return evaled;
+                if (typeof evaled === "number" && isFinite(evaled)) {
+                        return evaled;
+                }
+                else {
+                        console.error("Error while calculating the value!");
+                        return;
+                }
         }
         else {
-        	console.error("Error while calculating the value!");
-        	return;
+                return value;
         }
-	}
-	else {
-		return value;
-	}
 }
 
 module.exports.EVAL = Eval;
