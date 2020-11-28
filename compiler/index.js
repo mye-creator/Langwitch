21e8fa2c20a7a3afaf030266dd60b9467c6085d4
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-29 01:36:02 +0600

    Update index.js

--- compiler/index.js
+++ compiler/index.js
@@ -4,6 +4,8 @@ let dictionary = require("./modules/dictionary.js").DICTIONARY;
 let lexer = require("./modules/lexer/main.js").LEXER;
 let parser = require("./modules/parser/main.js").PARSER;
 
+global.vars = [];
+
 fs.readFile("../test/test.lw", "utf-8", function(error, content) {
   if (error === null) {
   	let lexems = lexer(content, dictionary);
@@ -11,12 +13,12 @@ fs.readFile("../test/test.lw", "utf-8", function(error, content) {
     let parsedString = parser(content, dictionary);
     
     console.log(parsedString);
-    
+
     // console.log(JSON.stringify(lexems, null, 4));
 
-  } else {
-
-    console.error("ERROR!!!")
-    console.error(error)
+  }
+  else {
+    console.error("ERROR!!!");
+    console.error(error);
   }
 });
