2544846141ffeb90b20910f2bb87c4c45b6f1732
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-29 01:44:38 +0600

    Update main.js

--- compiler/modules/parser/main.js
+++ compiler/modules/parser/main.js
@@ -1,28 +1,29 @@
 function parser (lexems, dictionary) {
-   let start = `#include <stdio.h>\n\nint main () {\n`;
-   let current = null;
-   let end = `return 0;\n}`;
+	let start = `#include <stdio.h>\n\nint main () {\n`;
+	let current = null;
+	let end = `return 0;\n}`;
 
-   if (lexems && lexems.length !== 0) {
-      for (let i = 0; i < lexems.length; i++) {
-         let functionName = lexems[i]["function"];
-         let valueObj = lexems[i]["value"];
-         if (lexems[i]["function"] && dictionary["function"][functionName.toLowerCase()]) {
-            let currentFunction = dictionary["function"][functionName.toLowerCase()];
+	if (lexems && lexems.length !== 0) {
+		for (let i = 0; i < lexems.length; i++) {
+			let functionName = lexems[i]["function"];
+			let valueObj = lexems[i]["value"];
 
-            current = currentFunction(valueObj);
-            start = start + current;
-         }
-         else {
-            console.error("Function \""+functionName+"\" isn't registered!");
-         }
-       }
+			if (lexems[i]["function"] && dictionary["function"][functionName.toLowerCase()]) {
+				let currentFunction = dictionary["function"][functionName.toLowerCase()];
 
-       return start + end;
-   }
-   else {
-    console.error("Array of lexems is empty!")
-   }
+				current = currentFunction(valueObj);
+				start = start + current;
+			}
+			else {
+				console.error("Function \""+functionName+"\" isn't registered!");
+			}
+		}
+
+		return start + end;
+	}
+	else {
+		console.error("Array of lexems is empty!");
+	}
 };
 
 module.exports.PARSER = parser;
