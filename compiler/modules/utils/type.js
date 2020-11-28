e58ab0f1dc5e0919fd9f096598be0ed6e3c019b5
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-28 22:09:36 +0600

    Create type.js

---
+++ compiler/modules/utils/type.js
@@ -0,0 +1,24 @@
+let Type = function (variable) {
+  if (typeof variable === "number") {
+    if (variable % 1 === 0) {
+      return "int";
+    }
+    else {
+      return "float";
+    }
+  }
+  else if (typeof variable === "string") {
+    if (variable.length === 1) {
+      return "char";
+    }
+    else {
+      return "string";
+    }
+  }
+  else {
+    console.error("Incorrect type : " + typeof variable);
+    return;
+  }
+};
+
+module.exports.TYPE = Type;
