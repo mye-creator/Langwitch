d665a1712b205e6760a352e1bb81de098e4c02f7
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-28 17:46:47 +0600

    Update and rename compiler/modules/parser.js to compiler/modules/parser/main.js

--- compiler/modules/parser/main.js
+++ compiler/modules/parser/main.js
@@ -1,18 +1,25 @@
 function parser (lexems, dictionary) {
+   let start = `#include <stdio.h>\n\nint main () {\n`;
+   let current = null;
+   let end = `return 0;\n}`;
+   
    if (lexems && lexems.length !== 0) {
       for (let i = 0; i < lexems.length; i++) {
-         let functionName = lexems[i]["function"]["function"] ? lexems[i]["function"] : lexems[i]["undefined_function"];
-         let valueObj = lexems[i]["value"] = dictionary["function"][functionName.toLowerCase()];
-         
+         let functionName = lexems[i]["function"];
+         let typeObj = lexems[i]["type"];
+         let valueObj = lexems[i]["value"];
          if (lexems[i]["function"] && dictionary["function"][functionName.toLowerCase()]) {
-            let curretnFunction = dictionary["function"][functionName.toLowerCase()];
+            let currentFunction = dictionary["function"][functionName.toLowerCase()];
             
-            currentFunction(valueObj.value);
+            current = currentFunction(typeObj.value);
+            start = start + current;
          }
          else {
             console.error("function \""+functionName+"\" isn't registered!");
          }
       }
+
+      return start + end;
    else {
       console.error("Array of lexems is empty");
    }
