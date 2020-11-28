e09c2c2dbe1ecce626a92504ffd1aa3338149c2d
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-29 00:11:44 +0600

    Update dictionary.js

--- compiler/modules/dictionary.js
+++ compiler/modules/dictionary.js
@@ -8,7 +8,7 @@ let DICTIONARY = {
        let parsedValue = Eval(val);
 
        if (Type(parsedValue) !== type) {
-          console.error("Type Error : " + typeof parsedValue + " !== " + type);
+          console.error("Type Error : " + type + " !== " + Type(parsedValue));
           return;
        }
        
@@ -22,8 +22,41 @@ let DICTIONARY = {
           return;
        }
        
-       return `printf(${typeSymbol},${parsedValue});\n`;
+       return `printf(${typeSymbol},${parsedValue});\n`;}
 
+    var : function (type, val) {
+       if (val.match(/\=/gm).length !== 1) {
+          console.error("Unknown syntax: " + val);
+          return;
+       }
+       else {
+          let varName = val.split("=")[0].trim();
+          let varValue = val.split("=")[1].trim();
+
+          if (Type(varvalue) !== type) {
+             console.error("Type Error: " + type + " !== " + Type(varValue));
+             return;
+          }
+          else {
+             if (/[^a-zA-Z0-9]/gm.test(varName)) {
+                console.error("There are unacceptable symbols in the variable name : " + varName);
+                return;
+             }
+             else {
+                if (/[0-9]/gm.test(varName[0])) {
+                   console.error("The first letter of the variable should not be a number : " + varName);
+                }
+                else {
+                   if (Type(varValue) === "string") {
+                      return `char ${varName}[]=${varValue};`;
+                   }
+                   else {
+                      return `${type} ${varName}=${varValue};`;
+                   }
+                }
+             }
+          }
+       }
     }
 
   },
