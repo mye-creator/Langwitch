00854345e91c87c92f1a577cf6123ef475201afc
Daniel Mye <68376637+mye-creator@users.noreply.github.com>
2020-11-28 19:13:58 +0600

    Update dictionary.js

--- compiler/modules/dictionary.js
+++ compiler/modules/dictionary.js
@@ -2,18 +2,28 @@ let DICTIONARY = {
   "function" : {
     print : function (type, val) {
        let typeSymbol = "%";
-      
+       let parsedValue = value;
+       
+       if (type === "int" || type === "float") {
+          parsedValue = eval(parsedValue);
+         
+          if (typeof parsedValue !== type) {
+             console.error("Type Error : " + typeof parsedValue + " !== " + type);
+             return;
+          }
+       }
+       
        if (type === "char") typeSymbol += "c";
        else if (el.type === "string") typeSymbol += "s";
        else if (el.type === "int") typeSymbol += "d";
        else if (el.type === "float") typeSymbol += "f";
-       else if (el.type === "undefined") typesymbol += "s";
+       else if (el.type === "undefined") typeSymbol += "s";
        else {
-          console.error("UNKNOWN_TYPE");
+          console.error("UNKNOWN_TYPE : " + type);
           return;
        }
        
-       return `printf(${typeSymbol},${val});\n`;
+       return `printf(${typeSymbol},${parsedValue});\n`;
 
     }
 
